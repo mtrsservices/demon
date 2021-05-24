795ff385fa4a7e4e008747ef2a2b443e70f796e5
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-05-24 19:00:08 -0400

    Add files via upload

---
+++ boot3D.swift
@@ -0,0 +1,115 @@
+var axioms:Set [int:String] = [1:"",2:"'x','x','x','y','y','y','z','z','z','x','y','x','y','x','z','z','x','y','z','z','y','y','y','z','xx','yy','zz','yx','yy','yz','zx','zy','zz','xxx','yyy','zzz','xyx','yzy','zyz','xzx','yzy','zyz','xxy','yyx','zzy','yxx','yyz','yzz'",3:"'x','x','x','y','y','y','z','z','z','x','y','x','y','x','z','z','x','y','z','z','y','y','y','z','xx','yy','zz','yx','yy','yz','zx','zy','zz','xxx','yyy','zzz','xyx','yzy','zyz','xzx','yzy','zyz','xxy','yyx','zzy','yxx','yyz','yzz'",4:"'x','x','x','y','y','y','z','z','z','x','y','x','y','x','z','z','x','y','z','z','y','y','y','z','xx','yy','zz','yx','yy','yz','zx','zy','zz','xxx','yyy','zzz','xyx','yzy','zyz','xzx','yzy','zyz','xxy','yyx','zzy','yxx','yyz','yzz'",5:""]
+
+	let Rotations: [int] = ["'0'","'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'","'9'","'10'","'11'","'12'","'13'","'14'","'15'","'16'","'17'","'18'","'19'","'20'","'21'","'22'","'23'","'24'","'25'","'26'","'27'","'28'","'29'","'30'","'31'","'32'","'33'","'34'","'35'","'36'","'37'"] 
+
+let dimensions : [int] = ["'0'","'1'","'2'","'3'"] 
+
+var isometries:Set
+[int:String:int]
+=
+["''":"":37
+'rotations':"+++":1,
+'dimensions':">>>":2,
+'rotations':"+++":3,
+'axioms':"*":4,
+'rotations':">>>":5,
+'dimensions':"+++":6,
+'rotations':">>>":7,
+'axioms':"*":8,
+'rotations':"+++":9,
+'dimensions':"<<<":10,
+'rotations':"+++":11,
+'axioms':"":12,
+'rotations':"<<<":13,
+'dimensions':"+++":14,
+'rotations':">>>":15,
+'axioms':"*":16,
+'rotations':">>>":17,
+'dimensions':"+++":18,
+'rotations':">>>":19,
+'axioms':"*":20,
+'rotations':"+++":21,
+'dimensions':"+++":22,
+'rotations':"+++":23,
+'axioms':"*":24,
+'rotations':"<<<":25,
+'dimensions':"<<<":26,
+'axioms':"<<<":27,
+'deminsions':"*":28,
+'rotations':"*":29,
+'axioms':"*":30,
+'dimensions':">>>":31,
+'rotations':">>>":32,
+'axioms':">>>":33,
+'dimensions':"=>>":34,
+'rotations':"&<<":35,
+'axioms':"",36]
+let off=isoMetries[36]
+switch on {
+case isoMetries[1]
+print(isoMetries[2])
+case isoMetries[2]
+print(isoMetries[3])
+case isoMetries[3]
+print(isoMetries[4])
+case isoMetries[4]
+print(isoMetries[5])
+case isoMetries[5]
+print(isoMetries[6])
+case isoMetries[6]
+print(isoMetries[7])
+case isoMetries[7]
+print(isoMetries[8])
+case isoMetries[8]
+print(isoMetries[9])
+case isoMetries[9]
+print(isoMetries[10])
+case isoMetries[11]
+print(isoMetries[12])
+case isoMetries[12]
+print(isoMetries[13])
+case isoMetries[13]
+print(isoMetries[14])
+case isoMetries[14]
+print(isoMetries[15])
+case isoMetries[15]
+print(isoMetries[16])
+case isoMetries[16]
+print(isoMetries[17])
+case isoMetries[17]
+print(isoMetries[18])
+case isoMetries[18]
+print(isoMetries[19])
+case isoMetries[19]
+print(isoMetries[20])
+case isoMetries[21]
+print(isoMetries[22])
+case isoMetries[22]
+print(isoMetries[23])
+case isoMetries[23]
+print(isoMetries[24])
+case isoMetries[24]
+print(isoMetries[25])
+case isoMetries[25]
+print(isoMetries[26])
+case isoMetries[26]
+print(isoMetries[27])
+case isoMetries[27]
+print(isoMetries[28])
+case isoMetries[28]
+print(isoMetries[29])
+case isoMetries[29]
+print(isoMetries[30])
+case isoMetries[31]
+print(isoMetries[32])
+case isoMetries[32]
+print(isoMetries[33])
+case isoMetries[33]
+print(isoMetries[34])
+case isoMetries[34]
+print(isoMetries[35])
+case isoMetries[35]
+print(isoMetries[37])
+default: 
+print(off)
+}
