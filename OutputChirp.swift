30daa7d6b9389e243ba7c0fae0079a728c293c90
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-05-27 04:11:33 -0400

    Add files via upload

---
+++ OutputChirp.swift
@@ -0,0 +1,87 @@
+
+func Chirp(
+var SourceOne:Dict[float:String]=
+[1.0:"Origin",
+2.33:"Constructive",
+3.66:"Destructive",
+4.99:"Recycle"]
+var SourceTwo:Dict[float:String]=
+[1.0:"Origin",
+2.33:"Constructive",
+3.66:"Destructive",
+4.99:"Recycle"]
+ 
+var InPointsOne:Dict;
+[int:String]=
+[1:"IntOrigin",
+2:"IntConstructive",
+3:"IntDestructive",
+4:"IntRecycle"]
+ 
+var InPointsTwo:Dict=
+[int:String]=
+[1:"IntOrigin",
+2:"IntConstructive",
+3:"IntDestructive",
+4:"IntRecycle"]
+
+var BPointsOneSrc
+[[float:String]:[float:String]:[float:String]:[float:String]]=
+[0.0:"Origin",
+0.11:"Constructive",
+0.55:"Destructive",
+0.77:"Recycle"]:
+[0.99:"Origin",
+1.11:"Constructive",
+1.55:"Destructive",
+1.77:"Recycle"]:
+[1.99:"Origin",
+2.11:"Constructive",
+2.55:"Destructive",
+2.77:"Recycle"]:
+[2.99:"Origin",
+3.11:"Constructive",
+3.55:"Destructive",
+3.77:"Recycle"]:
+ var BPointsTwoSrc
+[[float:String]:[float:String]:[float:String]:[float:String]]=
+[2.22:"Origin",
+2.44:"Constructive",
+2.66:"Destructive",
+2.88:"Recycle"]:
+[3.22:"Origin",
+3.44:"Constructive",
+3.66:"Destructive",
+3.88:"Recycle"]:
+[4.22:"Origin",
+4.44:"Constructive",
+4.66:"Destructive",
+4.88:"Recycle"]:
+
+func colorChirp() float!.count -> >>:BPointsTwoSrc[[_int,"Origin","Destructive","Constructive","Recycle"]->++3.3 ->>3.3]
+
+func sourceChirp() float!.count -> >>:SourceOne[_float,String]++>[[
+_float:"Origin"->_float:"Destructive",->>_float:"Constructive",-->_flot:"Recycle"]]
+<< <-:SourceTwo[_float,String]++>[[
+_float:"Origin"->_float:"Destructive",->>_float:"Constructive",-->_flot:"Recycle"]]
+
+func blendChirp() float!.count -> >>:BPointsOneSrc[_int,String]++>[[_int,"Origin","Destructive","Constructive","Recycle"]->++3.3 ->>3.3]]
+)
+{
+
+let ChirpOOne: func!.count = colorChirp(sourceOne[float:_String]) 
+print(ChirpOOne)
+
+let ChirpOTwo: func!.count = sourceChirp(sourceOne[float:_String]) 
+print(ChirpTwo)
+
+let  ChirpOThree: func!.count = blendChirp(sourceOne[float:_String])
+print(ChirpOThree)
+}
+if (IO = 0){
+return var IOCTL:double =
+IOCTL?.count (1.0++("{<on>""'::'""<off>}"))++=Chirp()
+} else {
+return var IOCTL?.count ??0==("{<on>""'::'""<off>}")===Chirp()
+}
+;
