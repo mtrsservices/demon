873c0760392eb690359273beb89e48b3ef1c61cc
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-05-27 04:40:59 -0400

    Update boot3D.swift

--- boot3D.swift
+++ boot3D.swift
@@ -1,3 +1,26 @@
+//MIT License
+
+//Copyright (c) 2021 mtrsservices
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+
 var axioms:Set [int:String] = [1:"",2:"'x','x','x','y','y','y','z','z','z','x','y','x','y','x','z','z','x','y','z','z','y','y','y','z','xx','yy','zz','yx','yy','yz','zx','zy','zz','xxx','yyy','zzz','xyx','yzy','zyz','xzx','yzy','zyz','xxy','yyx','zzy','yxx','yyz','yzz'",3:"'x','x','x','y','y','y','z','z','z','x','y','x','y','x','z','z','x','y','z','z','y','y','y','z','xx','yy','zz','yx','yy','yz','zx','zy','zz','xxx','yyy','zzz','xyx','yzy','zyz','xzx','yzy','zyz','xxy','yyx','zzy','yxx','yyz','yzz'",4:"'x','x','x','y','y','y','z','z','z','x','y','x','y','x','z','z','x','y','z','z','y','y','y','z','xx','yy','zz','yx','yy','yz','zx','zy','zz','xxx','yyy','zzz','xyx','yzy','zyz','xzx','yzy','zyz','xxy','yyx','zzy','yxx','yyz','yzz'",5:""]
 
 	let Rotations: [int] = ["'0'","'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'","'9'","'10'","'11'","'12'","'13'","'14'","'15'","'16'","'17'","'18'","'19'","'20'","'21'","'22'","'23'","'24'","'25'","'26'","'27'","'28'","'29'","'30'","'31'","'32'","'33'","'34'","'35'","'36'","'37'"] 
