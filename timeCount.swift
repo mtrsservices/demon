082a1900f05c68a182fe7dc8a9666e60b8da9b21
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-05-30 22:59:05 -0400

    Add files via upload

---
+++ timeCount.swift
@@ -0,0 +1,18 @@
+let second:float = .01
+let minute:float = .60
+let hour:double = 60.0
+let day:long = 1224.0
+let month:long = 3682.0
+let year:long = 44184.0
+
+func secToMin('.01'...'.60')
+func minToHour(.60...'60.0')
+func HourToDay(.60...'60.0')
+func DayToMonth('60.0'...'1224.0')
+func MonthToYear('1224.0'...'44184.0')
+
+func time(){func MonthToYear(){func DayToMonth(){func HourToDay(){func minToHour(){func secToMin)})})})}
+
+
+return func time()
+;
