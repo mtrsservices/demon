660199e338b60082c8eb05ae5cfb64e121677160
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-05-30 23:44:23 -0400

    Add files via upload

---
+++ chirp.swift
@@ -0,0 +1,20 @@
+import timeCount.swift 
+
+let IRFreqMinvisible:long = 4000000000000000
+let UVFreqMaxVisible:long = 8000000000000000
+let min =(IRFreqMinvisible) + double
+let max =(UVFreqMaxVisible) + double
+ func Chirp(){
+ for c in 400000000000000..<800000000000001{
+count(time)}
+ let cycle=count(Chirp())
+ switch cycle {
+ case uptime:
+ count(Chirp())
+ case downtime:
+ uptime.count(null)
+ default: 
+ print(uptime)
+ 
+
+return 0;
