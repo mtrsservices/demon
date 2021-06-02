2ef7e9a22bea51708ef29ea53744923c35cfb003
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-06-02 08:48:40 -0400

    Add files via upload

--- equalateralInterface.swift
+++ equalateralInterface.swift
@@ -1,32 +1,33 @@
 //namespace identifiers
-let  \displaystyle:int
-let  \partial:int
-let  \textrm:int
-let  \mathrm:int
-let  \ln = y=Mx+b
+let  "\\displaystyle";
+let  "\\partial";
+let  "\\textrm";
+let  "\\mathrm";
+let  "\\ln";
 //direction of orientation
-let  \left = \tan
-let  \right = \cos
+let  "\\left";
+let  "\\right";
 //fractions 
-let  \frac = 4\3.14
+let  "\\frac";
 //physical symbols
-let  \alpha;
-let  \omega;
-let  \theta;
-let  \Phi;
-let  \tau;
+let  "\\alpha";
+let  "\\omega";
+let  "\\theta";
+let  "\\Phi";
+let  "\\tau";
 //trig ids
-let  \sin = (3.14*1)/3.14)
-let  \cos = (3.14*3.14)/4)
-let  \tan = (3.14*1)/2)
+let  "\\sin";
+let  "\\cos";
+let  "\\tan";
 //GVD
-let GVD=={\displaystyle {\textrm {GVD}}(\omega _{0})\equiv {\frac {\partial }{\partial \omega }}\left({\frac {1}{v_{g}(\omega )}}\right)_{\omega =\omega _{0}},};
+let GVD=={\\displaystyle {\\textrm {GVD}}(\\omega _{0})\\equiv {\\frac {\\partial }{\\partial \\omega }}\\left({\\frac {1}{v_{g}(\\omega )}}\\right)_{\\omega =\\omega _{0}},};
 //GVDmd
-let GVDmd=={\displaystyle {\textrm {GVD}}(\omega _{0})\equiv \left({\frac {\partial ^{2}k}{\partial \omega ^{2}}}\right)_{\omega =\omega _{0}},};
+let GVDmd=={\\displaystyle {\\textrm {GVD}}(\\omega _{0})\\equiv \\left({\\frac {\\partial ^{2}k}{\\partial     \\omega ^{2}}}\\right)_{\\omega =     \\omega _{0}},};
 //opticalDensity
-let opticalDensity=T,{\displaystyle \tau =\ln \!\left({\frac {\Phi _{\mathrm {e} }^{\mathrm {i} }}{\Phi _{\mathrm {e} }^{\mathrm {t} }}}\right)=-\ln T,};
+let opticalDensity=T,{\\displaystyle \\tau =\\ln \\left({\\frac {\\Phi _{\\mathrm {e} }^{\\mathrm {i} }}{\\Phi _{\\mathrm {e} }^{\\mathrm {t} }}}\right)=-\\ln T,};
 //snellsLaw
-let snellsLaw2=0,{\displaystyle {\frac {\sin \theta _{2}}{\sin \theta _{1}}}={\frac {v_{2}}{v_{1}}}={\frac {n_{1}}{n_{2}}}};
+let snellsLaw2=0,{\\displaystyle {\\frac {\\sin \\theta _{2}}{\\sin      \\theta _{1}}}={\\frac {v_{2}}{v_{1}}}={\\frac {n_{1}}{n_{2}}}};
 //Schrödinger equation
 Schrödinger=
-t).{\displaystyle i\hbar {\frac {\partial }{\partial t}}\Psi (x,t)=\left[-{\frac {\hbar ^{2}}{2m}}{\frac {\partial ^{2}}{\partial x^{2}}}+V(x,t)\right]\Psi (x,t)\,.;
+t).{\\displaystyle i\\hbar {\\frac {  \\partial }{\\partial t}}\\Psi (x,t)=\\left[-{\\frac {\\hbar ^{2}}{2m}}{\\frac {\\partial ^{2}}{
+\\partial x^{2}}}+V(x,t)\\right]\\Psi (x,t)\,.;
