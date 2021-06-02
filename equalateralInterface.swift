d323c930357d1769c9e45ab7c9e38308df455802
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-06-02 03:52:24 -0400

    Update equalateralInterface.swift

--- equalateralInterface.swift
+++ equalateralInterface.swift
@@ -1,24 +1,24 @@
 //namespace identifiers
-let  "\displaystyle";
-let  "\partial";
-let  "\textrm";
-let  "\mathrm";
-let  "\ln";
+let  \displaystyle:int
+let  \partial:int
+let  \textrm:int
+let  \mathrm:int
+let  \ln = y=Mx+b
 //direction of orientation
-let  "\left";
-let  "\right";
+let  \left = \tan
+let  \right = \cos
 //fractions 
-let  "\frac";
+let  \frac = 4\3.14
 //physical symbols
-let  "\alpha";
-let  "\omega";
-let  "\theta";
-let  "\Phi";
-let  "\tau";
+let  \alpha;
+let  \omega;
+let  \theta;
+let  \Phi;
+let  \tau;
 //trig ids
-let  "\sin";
-let  "\cos";
-let  "\tan";
+let  \sin = (3.14*1)/3.14)
+let  \cos = (3.14*3.14)/4)
+let  \tan = (3.14*1)/2)
 //GVD
 let GVD=={\displaystyle {\textrm {GVD}}(\omega _{0})\equiv {\frac {\partial }{\partial \omega }}\left({\frac {1}{v_{g}(\omega )}}\right)_{\omega =\omega _{0}},};
 //GVDmd
