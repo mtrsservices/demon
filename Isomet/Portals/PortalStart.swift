846b715a455842694197a8edf19a06f90b56503a
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-06-07 09:10:16 -0400

    Create PortalStart.swift

---
+++ Isomet/Portals/PortalStart.swift
@@ -0,0 +1,16 @@
+let Portal = "Portal"
+
+func PortalStartLink(URL("./1"){
+  func start(){
+INSERT(ParametersArray[]=['0','...','100'])
+INSERT(MenuOptionsArray[]=['0','...','61'])
+INSERT(PermissionsArray[]=['0','...','17'])
+INSERT(UserArray[]=['0','1','2','3'])}
+if User = (UserAthenticated)
+then
+{return Start.append.UserArray[]=['4'] 
+else{
+  return print("User"+""+"Athentication"+""+"Failed")
+  
+       
+
