1959125755c507fe38730ab6526c353482e7e06d
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-06-07 10:11:27 -0400

    Update CIKernel.swift

--- Isomet/Kernel/CIKernel.swift
+++ Isomet/Kernel/CIKernel.swift
@@ -5,9 +5,4 @@ class CIKernel : NSObject{
 kernel vec4 do_nothing(sampler image) {
     vec2 dc = destCoord();
     return sample(image, samplerTransform(image, dc));
-}
-  func setROISelector( apply(extent:roiCallback:arguments:typealias CIKernelROICallback
-                            ))
-  func apply(extent: CGRect, roiCallback: CIKernelROICallback, arguments: [Any]) -> CIImage? ->
-  (CIImage? *)apply:(CIKernel *)k, ...;
-                     }
+};
